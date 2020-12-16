b3f01c806de804c5be50c45d9e8c61f80dfeaef6
lngka <foxlead@gmail.com>
2020-12-16 15:02:45 +0100

    q calculation

--- train_dec.py
+++ train_dec.py
@@ -8,8 +8,15 @@ tf.compat.v1.enable_eager_execution()
 
 ''' Step0: Get nice data
 '''
-data, labels = get_shuffled_data_set()
-data = np.expand_dims(data, -1)
+#data, labels = get_shuffled_data_set()
+#data = np.expand_dims(data, -1)
+tf.compat.v1.enable_eager_execution()
+
+# data = np.full(shape=(10, 400, 100, 1), fill_value=0.645)  # fake data to test
+#data = np.random.rand(10, 400, 100, 1)
+#np.save('./testdata.npy', data)
+labels = np.full(shape=(10,), fill_value=3)
+data = np.load('./testdata.npy')
 
 ''' Step1: Initialization
     1.1: load pre trained encoder to extract features from data
@@ -31,5 +38,5 @@ languageDEC.initialize(data)
     update_interval: 
 '''
 languageDEC.compile(optimizer='sgd', loss='kld')
-languageDEC.fit(x=data, y=labels, max_iteration=100,
-                update_interval=5, batch_size=100)
+languageDEC.fit(x=data, y=labels, max_iteration=1,
+                update_interval=1, batch_size=1)
