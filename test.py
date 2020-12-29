2399e42e7c2118c78e07dbe4632002944cea0b8b
lngka <foxlead@gmail.com>
2020-12-29 12:10:34 +0100

    MLClassifier test

--- test.py
+++ test.py
@@ -1,20 +1,39 @@
-from dataset import get_data_set
-from LanguageDEC import LanguageDEC
-from DECLayer import DECLayer
-import os
-import tensorflow as tf
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from dataset import get_shuffled_data_set
+import numpy as np
+from AutoEncoder import AutoEncoder
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-save_path = f'{dir_path}/models'
-encoder = tf.keras.models.load_model(f'{save_path}/dec_encoder')
-languageDEC = tf.keras.models.load_model(
-    f'{save_path}/dec', custom_objects={'DECLayer': DECLayer})
 
-languageDEC.summary()
+data, labels = get_shuffled_data_set()
+data = np.expand_dims(data, -1)
 
-dataset, classes = get_data_set(['de'])
+autoencoder = AutoEncoder()
+encoder = autoencoder.load_encoder()
 
-dataset = tf.expand_dims(dataset, -1)
+data = encoder.predict(data)
+print(data.shape)
 
-predictions = languageDEC.predict(dataset, steps=1)
-print(predictions.argmax(1))
+
+X_train, X_test, y_train, y_test = train_test_split(data, labels, stratify=labels,
+                                                    random_state=1, train_size=0.8)
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+autoencoder = AutoEncoder()
+encoder = autoencoder.load_encoder()
+
+
+clf = MLPClassifier(hidden_layer_sizes=[
+                    50, 5], random_state=1, max_iter=2048).fit(X_train, y_train)
+
+
+pred = clf.predict(X_test)
+print('Pred:', pred)
+print('y: ', y_test)
+
+score = clf.score(X_test, y_test)
+print(score)
