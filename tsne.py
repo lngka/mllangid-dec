3254e6362c0eeb15e9f12a61951ccb959fd6de19
lngka <foxlead@gmail.com>
2021-02-03 14:01:39 +0100

    plot & outliers removal

--- tsne.py
+++ tsne.py
@@ -5,7 +5,67 @@ from AutoEncoder_2 import AutoEncoder
 from sklearn import datasets
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 from matplotlib import pyplot as plt
+
+import scipy as sp
+import pandas as pd
+
+from scipy.stats import chi2
+from sklearn.covariance import MinCovDet
+
+
+def robust_mahalanobis_method(df):
+    # Minimum covariance determinant
+    rng = np.random.RandomState(0)
+    real_cov = np.cov(df.values.T)
+    X = rng.multivariate_normal(mean=np.mean(
+        df, axis=0), cov=real_cov, size=506)
+    cov = MinCovDet(random_state=0).fit(X)
+    mcd = cov.covariance_  # robust covariance metric
+    robust_mean = cov.location_  # robust mean
+    inv_covmat = sp.linalg.inv(mcd)  # inverse covariance metric
+
+    # Robust M-Distance
+    x_minus_mu = df - robust_mean
+    left_term = np.dot(x_minus_mu, inv_covmat)
+    mahal = np.dot(left_term, x_minus_mu.T)
+    md = np.sqrt(mahal.diagonal())
+
+    # Flag as outlier
+    outlier = []
+    # degrees of freedom = number of variables
+    C = np.sqrt(chi2.ppf((1-0.001), df=df.shape[1]))
+    for index, value in enumerate(md):
+        if value > C:
+            outlier.append(index)
+        else:
+            continue
+    return outlier, md
+
+
+def mahalanobis_method(df):
+    # M-Distance
+    x_minus_mu = df - np.mean(df)
+    cov = np.cov(df.values.T)  # Covariance
+    inv_covmat = sp.linalg.inv(cov)  # Inverse covariance
+    left_term = np.dot(x_minus_mu, inv_covmat)
+    mahal = np.dot(left_term, x_minus_mu.T)
+    md = np.sqrt(mahal.diagonal())
+
+    # Flag as outlier
+    outlier = []
+    # Cut-off point
+    # degrees of freedom = number of variables
+    C = np.sqrt(chi2.ppf((1-0.001), df=df.shape[1]))
+    for index, value in enumerate(md):
+        if value > C:
+            outlier.append(index)
+        else:
+            continue
+    return outlier, md
+
 
 PLOT_CENTROIDS = True
 PLOT_CUSTOM_CENTROIDS = True
@@ -45,13 +105,26 @@ if PLOT_CUSTOM_CENTROIDS:
         lang = languages[i]
         label = i
         lang_x = X[y == i, ]
+        is_inlier = IsolationForest(
+            random_state=0, max_features=50, contamination=0.2).fit_predict(lang_x)
+        print(is_inlier)
+
+        # is_inlier = LocalOutlierFactor(
+        #     n_neighbors=6, contamination=0.2).fit_predict(lang_x)
+
+        # df = pd.DataFrame(lang_x)
+        # outlier, md = robust_mahalanobis_method(df)
+        # is_inlier = np.ones(lang_x.shape[0], dtype=int)
+        # is_inlier[outlier] = 0
+
+        lang_x = lang_x[is_inlier == 1]
         lang_centroid = np.average(lang_x, axis=0)
         custom_centroids.append(lang_centroid)
     custom_centroids = np.array(custom_centroids)
 
-    kmeans = KMeans(n_clusters=2, init=custom_centroids)
-    kmeans.fit_predict(X)
-    custom_centroids = kmeans.cluster_centers_
+    #kmeans = KMeans(n_clusters=len(languages), init=custom_centroids)
+    # kmeans.fit_predict(X)
+    #custom_centroids = kmeans.cluster_centers_
 
     l_centroids = len(languages)
     n_centroids = custom_centroids.shape[0]
@@ -82,7 +155,7 @@ X_2d = tsne.fit_transform(X)
 indices = range(len(languages))
 
 plt.figure(figsize=(6, 5))
-colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
+colors = 'r', 'g', 'b', 'black', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
 for i, color, lang in zip(indices, colors, languages):
     z = 0
     m = 'o'
@@ -99,6 +172,6 @@ for i, color, lang in zip(indices, colors, languages):
                 c=color, label=lang, zorder=z, marker=m, s=size)
 
 
-plt.title('encoder_70_pretrained')
+plt.title('encoder_70_trained_iso_forest')
 plt.legend()
 plt.show()
