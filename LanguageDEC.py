dda03ed6a6be7d579290610bb90f69d389e4c5d3
lngka <foxlead@gmail.com>
2021-02-04 12:35:42 +0100

    missing import

--- LanguageDEC.py
+++ LanguageDEC.py
@@ -6,6 +6,7 @@ from sklearn.metrics.pairwise import euclidean_distances
 from tensorflow.keras.layers import Flatten
 from DECLayer import DECLayer
 import tensorflow as tf
+import pandas as pd
 import numpy as np
 import os
 
